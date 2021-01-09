1316c1ae19b93454880336bca3e79413071373e8
thebathroom <77169683+thebathroom@users.noreply.github.com>
2021-01-08 19:35:24 -0800

    Created base files
    Created Main
    Created Reference
    Created some packages

---
+++ src/main/java/GamarsMod/Main.java
@@ -0,0 +1,38 @@
+package GamarsMod;
+
+import GamarsMod.util.Reference;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid = Reference.MOD_ID, version = Reference.VERSION, name = Reference.MOD_NAME)
+public class Main {
+    @Mod.Instance
+    public static Main instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void serverInit(FMLServerStartingEvent event) {
+
+    }
+}
