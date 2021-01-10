1902af54ab50c933812aac77b175341d8ee950ca
thebathroom <77169683+thebathroom@users.noreply.github.com>
2021-01-10 14:44:29 -0800

    changed reinforced woolin pants texture and toughness
    tried to make reinforced woolin chest, head, and shoes but couldn't do the texture right

--- src/main/java/GamarsMod/init/ItemInit.java
+++ src/main/java/GamarsMod/init/ItemInit.java
@@ -28,7 +28,7 @@ public class ItemInit {
     //MATERIALS
     public static final Item.ToolMaterial TEST_TOOL = EnumHelper.addToolMaterial("test_tool", 3, 9001, 12.0F, 4.0F, 18);
     public static final ItemArmor.ArmorMaterial TEST_ARMOUR = EnumHelper.addArmorMaterial("test_armour", Reference.MOD_ID + ":TEST", 15, new int[]{3, 6, 8, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F);
-    public static final ItemArmor.ArmorMaterial REINFORCED_WOOLIN = EnumHelper.addArmorMaterial("reinforced_woolin", Reference.MOD_ID + ":REINFORCED_WOOLIN", 7, new int[]{4, 6, 7, 5}, 30, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 5.69F);
+    public static final ItemArmor.ArmorMaterial REINFORCED_WOOLIN = EnumHelper.addArmorMaterial("reinforced_woolin", Reference.MOD_ID + ":REINFORCED_WOOLIN", 7, new int[]{4, 6, 7, 5}, 30, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 3.0F);
 
     //ARMOUR
     public static final Item TEST_HELMET = new ArmourBase("test_helmet", TEST_ARMOUR, 1, EntityEquipmentSlot.HEAD);
