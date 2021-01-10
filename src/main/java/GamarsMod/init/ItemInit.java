2f1fab71ff96f044b322062acb94ebe8637e6b10
thebathroom <77169683+thebathroom@users.noreply.github.com>
2021-01-09 21:52:38 -0800

    +Reinforced Woolin Pants, yet to make the rest of the armor
    Json, texutres, and codes for reinforced woolin armor
    Added reinforced woolin armor material
    Added reinforced woolin pants but not the rest of the gear

--- src/main/java/GamarsMod/init/ItemInit.java
+++ src/main/java/GamarsMod/init/ItemInit.java
@@ -28,6 +28,7 @@ public class ItemInit {
     //MATERIALS
     public static final Item.ToolMaterial TEST_TOOL = EnumHelper.addToolMaterial("test_cool", 3, 1561, 8.0F, 3.0F, 10);
     public static final ItemArmor.ArmorMaterial TEST_ARMOUR = EnumHelper.addArmorMaterial("test_armour", Reference.MOD_ID + ":TEST", 15, new int[]{3, 6, 8, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F);
+    public static final ItemArmor.ArmorMaterial REINFORCED_WOOLIN = EnumHelper.addArmorMaterial("reinforced_woolin", Reference.MOD_ID + ":REINFORCED_WOOLIN", 7, new int[]{4, 6, 7, 5}, 30, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 5.69F);
 
     //ARMOUR
     public static final Item TEST_HELMET = new ArmourBase("test_helmet", TEST_ARMOUR, 1, EntityEquipmentSlot.HEAD);
@@ -44,6 +45,9 @@ public class ItemInit {
 
     //ROBERT ITEM
     public static final Item BONK_HAMMER = new ItemBase("bonk_hammer");
+
+    //ROBERT ARMOUR
+    public static final Item REINFORCED_WOOLIN_PANTS = new ArmourBase("reinforced_woolin_pants", REINFORCED_WOOLIN,2, EntityEquipmentSlot.LEGS );
 
     //ANGELO ITEM
 }
