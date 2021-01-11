11ae6943801c529f1bbed14de16d97f1ce199e9c
Likeabozz123 <Likeabozz123@users.noreply.github.com>
2021-01-10 16:24:16 -0800

    Added Netherite Recipes & Test Material Recipe
    Added shapeless recipe for netherite gear
    Added Test Material recipe

--- src/main/java/GamarsMod/recipes/CraftingRecipes.java
+++ src/main/java/GamarsMod/recipes/CraftingRecipes.java
@@ -11,12 +11,10 @@ public class CraftingRecipes {
 
     public static void init() {
 
-/*
         // TEST BLOCK -> TEST MATERIAL
         ItemStack test_material = new ItemStack(BlockInit.TEST_BLOCK);
         ItemStack test_ingot = new ItemStack(ItemInit.TEST_MATERIAL, 9);
         GameRegistry.addShapelessRecipe(new ResourceLocation("test_material"), null, test_ingot, Ingredient.fromStacks(test_material));
-*/
 
     }
 
