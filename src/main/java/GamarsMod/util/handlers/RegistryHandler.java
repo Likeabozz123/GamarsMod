166b27b5d7767d7848e1daa180be475ace972d64
Kelaiskool <77215657+Kelaiskool@users.noreply.github.com>
2021-01-10 22:19:41 -0800

    Added new crafting recipes
    -Added crafting recipes for all netherite armor
    -Added crafting recipes for all netherite tools
    -Added crafting recipe so you can turn netherite ingots into blocks and vise versa

--- src/main/java/GamarsMod/util/handlers/RegistryHandler.java
+++ src/main/java/GamarsMod/util/handlers/RegistryHandler.java
@@ -11,6 +11,7 @@ import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod.EventBusSubscriber
 public class RegistryHandler {
@@ -42,10 +43,8 @@ public class RegistryHandler {
         }
     }
 
-    public static void preInitRegisteries(){
-
+    public static void preInitRegisteries() {
     }
-
     public static void initRegisteries(){
         Main.proxy.render();
         SmeltingRecipes.init();
