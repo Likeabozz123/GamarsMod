e2711cd36e5e79790629117ed2ccd83495c449d5
Likeabozz123 <Likeabozz123@users.noreply.github.com>
2021-01-10 17:27:06 -0800

    Updated ToolAxe to fix crash temp

--- src/main/java/GamarsMod/objects/tools/ToolAxe.java
+++ src/main/java/GamarsMod/objects/tools/ToolAxe.java
@@ -11,18 +11,20 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
 import net.minecraft.item.ItemTool;
 
 import java.util.Set;
 
-public class ToolAxe extends ItemAxe {
+public class ToolAxe extends ItemSword {
 
-    private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON, Blocks.WOODEN_PRESSURE_PLATE, Blocks.CRAFTING_TABLE);
+    // private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON, Blocks.WOODEN_PRESSURE_PLATE, Blocks.CRAFTING_TABLE);
 
     public ToolAxe(String name, ToolMaterial material, float attackDamage, float attackSpeed) {
         super(material);
-        this.attackDamage = attackDamage - 1.0F;
-        this.attackSpeed = attackSpeed - 4.0F;
+        setHarvestLevel("pickaxe", 3);
+/*      this.attackDamage = attackDamage - 1.0F;
+        this.attackSpeed = attackSpeed - 4.0F;*/
         setUnlocalizedName(name);
         setRegistryName(name);
         setCreativeTab(Main.gamarsTab);
@@ -30,9 +32,9 @@ public class ToolAxe extends ItemAxe {
         ItemInit.ITEMS.add(this);
     }
 
-    public float getStrVsBlock(ItemStack stack, IBlockState state) {
+/*    public float getStrVsBlock(ItemStack stack, IBlockState state) {
         Material material = state.getMaterial();
         return material != Material.WOOD && material != Material.PLANTS && material != Material.VINE ? 1 : this.toolMaterial.getEfficiency();
-    }
+    }*/
 
 }
